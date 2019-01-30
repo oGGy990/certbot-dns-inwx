6b81ae80686615446400708f8ff83998ed34b935
Oliver Ney <oliver@dryder.de>
2019-01-30 21:09:35 +0100

    Prepare setup.py for PyPI upload

--- setup.py
+++ setup.py
@@ -18,10 +18,15 @@ data_files = [
 	('/etc/letsencrypt', ['inwx.cfg'])
 ]
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='certbot-dns-inwx',
     version='2.1.0',
     description="INWX DNS Authenticator plugin for Certbot",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url='https://github.com/oGGy990/certbot-dns-inwx',
     author="Oliver Ney",
     author_email='oliver@dryder.de',
@@ -35,5 +40,16 @@ setup(
             'dns-inwx = certbot_dns_inwx.dns_inwx:Authenticator',
         ],
     },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: System Administrators",
+        "Topic :: Internet :: Name Service (DNS)",
+        "Topic :: Security :: Cryptography",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Plugins",
+        "Operating System :: OS Independent",
+    ]
 )
 
